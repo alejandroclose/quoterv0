ea2aa97c374e6483191005906faf39fb78eecb02
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-10-21 18:26:36 +0200

    dev branch

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Header = (props) => {
+  return <div>
+    <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>{props.children}</h1>
+        </header>
+        </div>
+
+}
+export default Header
