954b1d1e0fa877fc80782a19c076d24415fb6b02
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-11-06 19:01:05 +0100

    continued with css

--- src/components/QuoteProducts.js
+++ src/components/QuoteProducts.js
@@ -39,7 +39,7 @@ class QuoteProducts extends Component {
   render() {
     return (
       <div>
-        <div className="header">
+        <div className="product-picker">
 
           {this.state.productPickers.map((productPicker, index) => {
             return (<div key={index} onChange={this.handleOnChange}>
