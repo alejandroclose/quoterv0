2470cecb6ef5abb00fe8cff512e87ca2600d4db8
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-11-07 21:40:34 +0100

    started styles for product lines

--- src/components/QuoteProducts.js
+++ src/components/QuoteProducts.js
@@ -5,41 +5,57 @@ import Products from "../lib/products-service";
 class QuoteProducts extends Component {
   state = {
     products: []
-  }
+  };
 
-  handleData = data => {
+  handleData = data => {
     this.getProduct(data);
-  }
+  };
 
-  getProduct = (id) => {
-    Products.getProduct(id)
-      .then((product)=> {
-        const { products } = this.state;
-        products.push(product)
-        this.setState({
-          products: products,
-        })
-      })
-  }
+  getProduct = id => {
+    Products.getProduct(id).then(product => {
+      const { products } = this.state;
+      products.push(product);
+      this.setState({
+        products: products
+      });
+    });
+  };
 
-  render(){
-    
-    return(<div>
+  render() {
+    return (
       <div>
-        <ProductPicker sendData={this.handleData}/>
+        {/* Products returned after using the product picker */}
+        <div>
+          {this.state.products.map(product => {
+            return (
+             
+              <li className="quote-product-line" key={product._id}>
+                <div className="quote-product-info">
+                  <div className="quote-product-image media-left"><img src={product.image} alt="product badge"/></div>
+                  <div>{product.name}</div>
+                    {product.price}
+                    {product.currency}
+                  </div>
+              </li>
+           
+            );
+          })}
+        </div>
+        <div>
+        <div>
+          <ProductPicker sendData={this.handleData} />
+        </div>
+
+        {/* Sum of product's price */}
+        <div>
+          {this.state.products.reduce((acc, product) => {
+            console.log('hola')
+            return acc + product.price;
+          }, 0)}
+        </div>
       </div>
-      <div>
-        {this.state.products.map((product)=>{
-          return <li key={product._id}>{product.name}</li>
-        })}
       </div>
-      {/* Sum of products prices */}
-      <div style={{margin: "20px"}}>
-        {this.state.products.reduce((acc, product) => {
-          return acc + product.price
-        }, 0)}
-      </div>
-    </div>)
+    );
   }
 }
 export default QuoteProducts;
