a3fa60d4d74de73820b653161faad47ea2d9b21e
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-11-08 21:04:37 +0100

    WIP edit-quote

--- src/pages/Edit-Quote.js
+++ src/pages/Edit-Quote.js
@@ -1,101 +1,214 @@
-import React, { Component} from 'react';
-import Quote from '../lib/quotes-service';
-import { withAuth } from '../components/AuthProvider'
-import Header from '../components/Header';
+import React, { Component } from "react";
+import Quote from "../lib/quotes-service";
+import { withAuth } from "../components/AuthProvider";
+import Header from "../components/Header";
+import Products from "../lib/products-service";
+import QuoteProducts from '../components/QuoteProducts';
 
 class EditQuote extends Component {
   state = {
-    name: '',
-    customer_name: '',
-    customer_address: '',
-    customer_email: '',
-    products:[],
+    name: "",
+    customer_name: "",
+    customer_address: "",
+    customer_email: "",
+    productsArr: [],
+    products: [],
     isLoading: false
-  }
+  };
 
-  componentDidMount(){
-    const {id} = this.props.match.params
+  componentDidMount = () => {
+    const { id } = this.props.match.params;
 
     Quote.getQuote(id)
-    .then((data) => {
-      const {name, customer_name, customer_address, customer_email, products} = data;
+      .then(data => {
+        const {
+          name,
+          customer_name,
+          customer_address,
+          customer_email,
+          products
+        } = data;
 
-      this.setState({
-        name,
-        customer_name: customer_name || '', 
-        customer_address: customer_address || '' ,
-        customer_email: customer_email || '' ,
-        products
+        this.setState({
+          name,
+          customer_name: customer_name || "",
+          customer_address: customer_address || "",
+          customer_email: customer_email || "",
+          productsArr: products || []
+        });
+        this.handleData();
       })
-    })
-    .catch((err)=>{
-      console.error(err)
-    })
-  }
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
-  handleOnChange = (e) => {
+  handleOnChange = e => {
     this.setState({
-      [e.target.name]: e.target.value,
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const {id} = this.props.match.params;
-    const { name, customer_name, customer_address, customer_email} = this.state;
-    Quote.editQuote(id, {name, customer_name, customer_address, customer_email})
-    .then ((result) => {
-      this.props.history.push(`/quotes`)
-    })
-    .catch((error) => {
-      console.error(error);
+      [e.target.name]: e.target.value
     });
-  }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { id } = this.props.match.params;
+    const {
+      name,
+      customer_name,
+      customer_address,
+      customer_email
+    } = this.state;
+    Quote.editQuote(id, {
+      name,
+      customer_name,
+      customer_address,
+      customer_email
+    })
+      .then(result => {
+        this.props.history.push(`/quotes`);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  handleData = () => {
+    this.state.productsArr.map(id => {
+      this.getProduct(id);
+      return console.log("hola");
+    });
+  };
+
+  getProduct = id => {
+    console.log("id", id);
+    Products.getProduct(id).then(product => {
+      console.log("product", product);
+      const { products } = this.state;
+      const newProducts = products;
+      newProducts.push(product);
+      this.setState({
+        products: newProducts
+      });
+    });
+  };
+
+  showProducts = () => {
+    this.state.products.map((product, index) => {
+      return (
+        <li className="quote-product-line" key={index}>
+          <div className="quote-product-info">
+            <div className="quote-product-image media-left">
+              <img src={product.image} alt="product badge" />
+            </div>
+            <div>{product.name}</div>
+            <div className="quote-product-price">
+              <div>{product.price}</div>
+              <div>{product.currency}</div>
+            </div>
+          </div>
+        </li>
+      );
+    });
+  };
 
   render() {
-    const { name, customer_name, customer_address, customer_email} = this.state;
+    const {
+      name,
+      customer_name,
+      customer_address,
+      customer_email
+    } = this.state;
     return (
       <div>
-        <Header/>
+        <Header />
+        <div className="quote-buttons">
+          <button
+            className="button is-success is-outlined"
+            onClick={() => this.handleSubmit()}
+          >
+            Save Quote
+          </button>
+        </div>
+        <div className="quote-template">
+          <div className="quote-header">
+            <div className="company-logo">
+              <img src="/images/noun_thunder_434559.png" alt="company logo" />
+            </div>
+            <div className="quote-title">QUOTE</div>
+          </div>
+          <div className="quote-sub-header">
+            <div className="company-info">
+              <p>THOR SL</p>
+              <p>C/Pamplona 96</p>
+              <p>08018 Barcelona</p>
+              <p>www.thor.es</p>
+            </div>
+            <div className="quote-info">
+              <div className="customer-info">
+                <input
+                  type="text"
+                  value={name}
+                  name="name"
+                  onChange={this.handleOnChange}
+                />
+                <input
+                  type="text"
+                  value={customer_name}
+                  name="customer_name"
+                  onChange={this.handleOnChange}
+                />
+                <input
+                  type="text"
+                  value={customer_address}
+                  name="customer_address"
+                  onChange={this.handleOnChange}
+                />
+                <input
+                  type="text"
+                  value={customer_email}
+                  name="customer_email"
+                  onChange={this.handleOnChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="quote-products">
+            <ul>
+              {console.log("desde view", this.state.productsArr)}
+              {console.log(this.state.products)}
+              {this.showProducts}
+              {this.state.products.map((product, index) => {
+                console.log(product);
+                return (
+                  <li className="quote-product-line" key={index}>
+                    <div className="quote-product-info">
+                      <div className="quote-product-image media-left">
+                        <img src={product.image} alt="product badge" />
+                      </div>
+                      <div>{product.name}</div>
+                      <div className="quote-product-price">
+                        <div>{product.price}</div>
+                        <div>{product.currency}</div>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+
+        {/* <Header/>
         <form onSubmit={this.handleSubmit}>
         <input type="text" value={name} name="name" onChange={this.handleOnChange}/>
         <input type="text" value={customer_name} name="customer_name" onChange={this.handleOnChange}/>
         <input type="text" value={customer_address} name="customer_address" onChange={this.handleOnChange}/>
         <input type="text" value={customer_email} name="customer_email" onChange={this.handleOnChange}/>
         <button type="submit">Save</button>
-        </form>
+        </form> */}
       </div>
-    )
+    );
   }
 }
 
 export default withAuth()(EditQuote);
-
-/// Not mine
-
-
-// render() {
-//   const { name, last_name, email, number, adress } = this.state
-//   return (
-//       <div>
-//          <h1 className="register-title">Edit Patient</h1>
-//         <form onSubmit={this.handleSubmit} className="register">
-//           <label className="label" htmlFor="">Name</label>
-//           <input className="register-input" type="text" value={name} name="name" onChange={this.handleOnChange} placeholder="Name"/>
-//           <label className="label" htmlFor="">Last name</label>
-//           <input className="register-input" type="text" value={last_name} name="last_name" onChange={this.handleOnChange} placeholder="Last Name"/>
-//           <label className="label " htmlFor="">Email</label>
-//           <input className="register-input" type="text" value={email} name="email" onChange={this.handleOnChange} placeholder="Email"/>
-//           <label className="label" htmlFor="">Phone number</label>
-//           <input className="register-input"  type="number" value={number} name="number" onChange={this.handleOnChange} placeholder="Number"/>
-//           <label className="label" htmlFor="">Adress</label>
-//           <input className="register-input" type="text" value={adress} name="adress" onChange={this.handleOnChange} placeholder="Adress" />
-//           <input className="register-button" type="submit" value="Update" />
-//         </form>
-//       </div>
-//   )
-// }
-// }
-// export default withRouter(FormEditPatient);
-
-// ////Not Mine
