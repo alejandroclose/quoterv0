2ecd66ccb7461e4ee921e61e175d7c690d4d415f
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-11-14 18:08:54 +0100

    fixed public bug

--- src/lib/public-products-service.js
+++ src/lib/public-products-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class PublicProductsApi {
   constructor() {
     this.publicProductsApi = axios.create({
-      baseURL: "http://localhost:5000/quote",
+      baseURL: "https://quoterapp.herokuapp.com/quote",
       withCredentials: true
     });
   }
